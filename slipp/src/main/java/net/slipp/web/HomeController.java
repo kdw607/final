e616ba29885fb9b12c0ef7e006bf355f73df32df
Administrator <Administrator@MSDN-SPECIAL>
2017-06-05 23:10:55 +0900

    6-1

--- slipp/src/main/java/net/slipp/web/HomeController.java
+++ slipp/src/main/java/net/slipp/web/HomeController.java
@@ -1,15 +1,21 @@
 package net.slipp.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String home(){
-
+		log.debug("success!!");
 		return "home";
 	}
 }
