9f39202319a97e3a21c3a1085de4b1ab5820ff9a
Administrator <Administrator@MSDN-SPECIAL>
2017-06-06 16:08:51 +0900

    10-2

--- slipp/src/main/java/net/slipp/web/HomeController.java
+++ slipp/src/main/java/net/slipp/web/HomeController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-	
 	
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
