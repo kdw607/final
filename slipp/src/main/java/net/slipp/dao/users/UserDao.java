01df283f52383719ad50de0efa81f43c04c6b0b4
Administrator <Administrator@MSDN-SPECIAL>
2017-06-06 14:06:21 +0900

    9-2

--- slipp/src/main/java/net/slipp/dao/users/UserDao.java
+++ slipp/src/main/java/net/slipp/dao/users/UserDao.java
@@ -1,8 +1,14 @@
 package net.slipp.dao.users;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.annotation.PostConstruct;
 
+import net.slipp.domain.users.User;
+
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -14,5 +20,22 @@ public class UserDao extends JdbcDaoSupport{
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
 		populator.addScript(new ClassPathResource("slipp.sql"));
 		DatabasePopulatorUtils.execute(populator, getDataSource());
+	}
+
+	public User findById(String userId) {
+		
+		String sql = "select * from users where userId = ?";
+		
+		RowMapper<User> rowMapper = new RowMapper<User>(){
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new User(
+							rs.getString("userId"),
+							rs.getString("password"),
+							rs.getString("name"),
+							rs.getString("email"));
+			}
+		};
+		return getJdbcTemplate().queryForObject(sql, rowMapper, userId);
 	}
 }
---
+++ slipp/src/test/java/net/slipp/dao/users/UserDaoTest.java
@@ -0,0 +1,28 @@
+package net.slipp.dao.users;
+
+import net.slipp.domain.users.User;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class UserDaoTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(UserDaoTest.class);
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+	public void findById() {
+		User user = userDao.findById("javajigi");
+		log.debug("User : {}", user);
+	}
+
+}
