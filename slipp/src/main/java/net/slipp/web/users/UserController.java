e7ded1338413a196a8599605e9cd85b68c99e719
Administrator <Administrator@MSDN-SPECIAL>
2017-06-06 16:34:40 +0900

    11-1

--- slipp/src/main/java/net/slipp/web/users/UserController.java
+++ slipp/src/main/java/net/slipp/web/users/UserController.java
@@ -14,21 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/users")
 public class UserController {
 
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-
-	@Autowired
-	private UserDao userDao;
-	
 	@RequestMapping("/form")
 	public String form(){
 		return "users/form";
 	}
-	
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public String create(User user){
-		log.debug("User : {}", user);
-		userDao.create(user);
-		log.debug("Database : {}", userDao.findById(user.getUserId()));
-		return "users/form";
-	}
+
 }
