037746eca548d702b717b8371b70298aa2ded918
kdw607 <kdweboy@hanmail.net>
2017-06-15 14:33:33 +0900

    23-2

--- slipp/src/test/java/net/slipp/dao/MyBatisTest.java
+++ slipp/src/test/java/net/slipp/dao/MyBatisTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-
+import static org.hamcrest.CoreMatchers.*;
 import net.slipp.dao.users.UserDaoTest;
 import net.slipp.domain.users.User;
 
@@ -12,6 +12,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,19 +20,31 @@ import org.slf4j.LoggerFactory;
 public class MyBatisTest {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserDaoTest.class);
+	
+	private SqlSessionFactory sqlSessionFactory;
 
-	@Test
-	public void gettingStarted() throws Exception {
+	@Before
+	public void setup() throws IOException {
 		String resource = "mybatis-config-test.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	
+	@Test
+	public void gettingStarted() {
+		try(SqlSession session = sqlSessionFactory.openSession()) {
 			User user = session.selectOne("UserMapper.findById", "javajigi");
 			log.debug("User : {}", user);
-		} finally {
-		  session.close();
+		}
+	}
+	
+	@Test
+	public void insert() throws Exception{
+		try(SqlSession session = sqlSessionFactory.openSession()) {
+			User user = new User("kdw607", "pass1234", "±Ç´ë¿ø", "kdw607@naver.com");
+			session.insert("UserMapper.create", user);
+			User dbuser = session.selectOne("UserMapper.findById", user.getUserId());
+			assertThat(dbuser, is(user));
 		}
 	}
 }
