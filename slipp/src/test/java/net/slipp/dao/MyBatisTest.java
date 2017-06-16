3495058eb0b7974b4b745f273b2da4bbe0914ab1
kdw607 <kdweboy@hanmail.net>
2017-06-16 10:08:02 +0900

    24-1

--- slipp/src/test/java/net/slipp/dao/MyBatisTest.java
+++ slipp/src/test/java/net/slipp/dao/MyBatisTest.java
@@ -4,10 +4,15 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import javax.sql.DataSource;
+import javax.websocket.RemoteEndpoint.Basic;
+
 import static org.hamcrest.CoreMatchers.*;
 import net.slipp.dao.users.UserDaoTest;
 import net.slipp.domain.users.User;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -16,6 +21,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 public class MyBatisTest {
 	
@@ -28,8 +36,22 @@ public class MyBatisTest {
 		String resource = "mybatis-config-test.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("slipp.sql"));
+		DatabasePopulatorUtils.execute(populator, getDataSource());
+		log.info("database initialized success!");
 	}
 	
+	private DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:~/slipp");
+		dataSource.setUsername("sa");
+
+		return dataSource;
+	}
+
 	@Test
 	public void gettingStarted() {
 		try(SqlSession session = sqlSessionFactory.openSession()) {
