2f392ebd11501d8077a7804b990070f24e500672
kdw607 <kdweboy@hanmail.net>
2017-06-16 16:18:01 +0900

    27-1

--- slipp/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
+++ slipp/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
@@ -1,7 +1,6 @@
 package net.slipp.dao.users;
 
 import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
@@ -14,26 +13,10 @@ import net.slipp.domain.users.User;
 
 public class MyBatisUserDao implements UserDao {
 
-	private static final Logger log = LoggerFactory.getLogger(MyBatisUserDao.class);
-	
 	private SqlSession sqlSession;
-	
-	private DataSource dataSource;
-	
-	@PostConstruct
-	public void initialize() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("slipp.sql"));
-		DatabasePopulatorUtils.execute(populator, dataSource);
-		log.info("database initialized success!");
-	}
-	
+
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
-	}
-	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
 	}
 	
 	@Override
