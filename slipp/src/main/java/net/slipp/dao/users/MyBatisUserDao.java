7b57d6391aa7d8ebca9cf815352a419fa70b511c
kdw607 <kdweboy@hanmail.net>
2017-06-16 17:52:34 +0900

    29-1

--- slipp/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
+++ slipp/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
@@ -5,14 +5,18 @@ import javax.annotation.PostConstruct;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Repository;
 
 import net.slipp.domain.users.User;
 
+@Repository
 public class MyBatisUserDao implements UserDao {
 
+	@Autowired
 	private SqlSession sqlSession;
 
 	public void setSqlSession(SqlSession sqlSession) {
